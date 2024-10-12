8cc1aa0f3d36756364d3537a7f16d88c980a5756
Jatin Chhabra <cjatin2822@gmail.com>
2024-10-13 00:12:28 +0530

    parse api version

--- src/main.rs
+++ src/main.rs
@@ -35,34 +35,22 @@ fn main() {
     }
 }
 
-fn parse_bytes_to_uint(byte_array: &[u8]) -> usize {
-    let mut result: usize = 0;
-    let last_element_idx = byte_array.len() - 1;
-    let idx = 0;
-
-    for byte in byte_array {
-        result = result + (*byte as usize) * (last_element_idx - idx) * 256;
-    }
-
-    result
-}
-
-// debug show function
-fn show(bs: &[u8]) -> () {
-    println!("{}", String::from_utf8_lossy(bs).into_owned());
-}
-
 fn parse_request_headers(mut stream: &TcpStream) -> RequestHeader {
+    // read bytes that represent length
     let mut length: [u8; 4] = [0; 4];
     stream.read_exact(&mut length);
+
+    // declare and initialize byte arrays for necessary fields
     let mut request_api_version: [u8; 2] = [0; 2];
     let mut correlation_id: [u8; 4] = [0; 4];
     let mut request_api_key: [u8; 2] = [0; 2];
 
+    // read bytes into those byte arrays
     stream.read_exact(&mut request_api_key);
     stream.read_exact(&mut request_api_version);
     stream.read_exact(&mut correlation_id);
 
+    // initialize RequestHeader enum
     RequestHeader {
         request_api_key: i16::from_be_bytes(request_api_key),
         request_api_version: i16::from_be_bytes(request_api_version),
@@ -74,10 +62,16 @@ fn parse_request_headers(mut stream: &TcpStream) -> RequestHeader {
 fn handle_connection(mut stream: &TcpStream) {
     let headers: RequestHeader = parse_request_headers(stream);
     let correlation_id: i32 = headers.correlation_id;
-    let response_size: [u8; 4] = [0, 0, 0, 4];
-    let response: Vec<u8> = [response_size, correlation_id.to_be_bytes()].concat();
-    match stream.write_all(&response) {
-        Ok(_) => (),
-        Err(_) => println!("Some error occured"),
+    let mut size: i32 = 4;
+    let mut error_code: i16 = 0;
+
+    // check if api version is invalid
+    if (headers.request_api_version < 0 || headers.request_api_version > 4) {
+        size += 2;
+        error_code = 35;
     }
+
+    stream.write_all(&size.to_be_bytes()); // size of resposne
+    stream.write_all(&correlation_id.to_be_bytes()); // correlation id as bytes in big endian
+    stream.write_all(&error_code.to_be_bytes());
 }
