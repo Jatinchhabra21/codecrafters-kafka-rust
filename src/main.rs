d8256580d5d751be3bcc71787357c24002d66dcc
Jatin Chhabra <cjatin2822@gmail.com>
2024-10-16 06:37:23 +0530

    added pattern matching for stream reading into vector

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,12 @@ fn main() {
 fn handle_connection(mut stream: &TcpStream) {
     println!("in handle_connection fn");
     let mut header_bytes: Vec<u8> = Vec::new();
-    stream.read_to_end(&mut header_bytes).unwrap();
+
+    match stream.read_to_end(&mut header_bytes) {
+        Ok(_) => (),
+        Err(error) => println!("Something went wrong! {:?}", error),
+    }
+
     println!("header bytes: {:?}", header_bytes);
     let headers: RequestHeader = RequestHeader::new(header_bytes);
     let size: i32 = 19;
